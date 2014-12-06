f95de3d77dee4fff7801bb77fc9b0d0c00d68d46
Gede Lumbung <gedesumawijaya@gmail.com>
2014-12-06 19:24:09 +0800

    fix footer

--- themes/abound/views/layouts/tpl_footer.php
+++ themes/abound/views/layouts/tpl_footer.php
@@ -2,7 +2,7 @@
         <div class="subnav navbar navbar-inverse">
             <div class="navbar-inner">
                 <div class="container">
-                    Designed by Mandar Network Solutions. All Rights Reserved<br>
+                    Designed by <a href="http://gedelumbung.com" target="_blank">DLMBG</a>. All Rights Reserved<br>
                     <?php echo Yii::app()->name; ?>
                 </div>
             </div>
